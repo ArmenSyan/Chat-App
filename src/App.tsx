9874ed0c3bbb71dc14657a4ebab476e20f661024
ArmenSyan <armen.sahakyan.ait@gmail.com>
2025-10-13 21:28:28 +0400

    Installed Firebase, and created btn to sign up with Google

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom"
+import SignIn from "./Components/Sign-in/SignIn"
+import Chat from "./Components/Chat/Chat"
+import SignUp from "./Components/Sign-up/SignUp"
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useState } from "react";
+import { auth } from './firebase';
 function App() {
-
+  const [user] = useAuthState(auth)
   return (
     <>
+      <Routes>
+
+        <Route path="/Sign-in" element={<SignIn />} />
+        <Route path="/Sign-up" element={<SignUp />} />
+        <Route path="/" element={<Chat />} />
+
+      </Routes>
     </>
   )
 }
---
+++ src/firebase.tsx
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAuth, signInWithPopup ,GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAlyhIlCfwmw9Z507212LkopMklChcYWgY",
+  authDomain: "chat-app-59512.firebaseapp.com",
+  databaseURL: "https://chat-app-59512-default-rtdb.firebaseio.com",
+  projectId: "chat-app-59512",
+  storageBucket: "chat-app-59512.firebasestorage.app",
+  messagingSenderId: "334812514975",
+  appId: "1:334812514975:web:baeef7e7a3e779267eb0aa",
+  measurementId: "G-JFYYZ9MZEG"
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export { signInWithPopup };
