8b12e42e58ba94060a15249ea689a3569ad34c0f
ArmenSyan <armen.sahakyan.ait@gmail.com>
2025-10-14 02:43:45 +0400

    fixed some bugs, and added styles in hover

--- src/Components/Sign-up/WithEmail.tsx
+++ src/Components/Sign-up/WithEmail.tsx
@@ -15,16 +15,19 @@ function WithEmail() {
     }
   };
   return (
-    <div>
+    <div className="flex flex-col justify-evenly items-center gap-y-[20px]">
         <input type="text"
+        className="w-[340px] h-[58px] px-[16px] text-p placeholder:font-normal placeholder:text-plh rounded-[7px] border-[1px] border-plh/50 focus:outline-[1px] focus:outline-plh/70 font-medium "
         onChange={(e) => setEmail(e.target.value)}
         placeholder="Enter Email" 
         value={email}/>
         <input type="password"
+        className="w-[340px] h-[58px] px-[16px] text-p placeholder:font-normal placeholder:text-plh rounded-[7px] border-[1px] border-plh/50 focus:outline-[1px] focus:outline-plh/70 font-medium "
         onChange={(e) => setPassword(e.target.value)}
         placeholder="Enter Password"
         value={password} />
         <button
+        className="w-[300px] h-[58px] bg-bg rounded-[30px] text-formBg font-medium text-[18px] hover:cursor-pointer hover:shadow-p/30 hover:shadow-md duration-300 transition-all"
         onClick={handleSignUp}>Sign Up</button>
 
     </div>
--- src/Components/Sign-up/WithGoogle.tsx
+++ src/Components/Sign-up/WithGoogle.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { auth, provider, signInWithPopup } from "../../firebase"
 import { UserCredential } from "firebase/auth";
 
@@ -11,10 +12,10 @@ function WithGoogle() {
         }
     };
     return (
-        <div>
+        <div className="flex flex-col justify-evenly items-center gap-y-[20px]">
             <button onClick={signInWithGoogle}
-            className="w-[300px] h-[58px] bg-bg rounded-[30px] text-formBg font-medium text-[18px] hover:cursor-pointer">Sign up with Google</button>
-
+            className="w-[300px] h-[58px] bg-formBg rounded-[30px] border-[2px] border-bg hover:shadow-p/30 hover:shadow-md text-h1 font-medium text-[18px] hover:cursor-pointer duration-300 transition-all">Sign up with Google</button>
+            <p className="text-[15px] text-p font-medium ">Already have an account? <Link to={'/sign-in'} className="font-semibold text-h1 underline">Log in</Link></p>
         </div>
     )
 }
