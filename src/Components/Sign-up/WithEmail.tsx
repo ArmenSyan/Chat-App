2888155078f2f55b7958fc1eb3f487a89db96010
ArmenSyan <armen.sahakyan.ait@gmail.com>
2025-10-15 15:06:41 +0400

    Added users name input with functionality

--- src/Components/Sign-up/WithEmail.tsx
+++ src/Components/Sign-up/WithEmail.tsx
@@ -1,35 +1,52 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
 import { auth } from "../../firebase";
 import { useNavigate } from "react-router-dom";
 
 function WithEmail() {
+  const [username, setUsername] = useState<string>(""); 
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const navigate = useNavigate()
   const handleSignUp = async () => {
     try {
+      // 1️⃣ Создаём пользователя
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      console.log("✅ User created:", userCredential.user);
-      navigate('/sign-in')
+      const user = userCredential.user;
+
+      // 2️⃣ Добавляем имя
+      await updateProfile(user, { displayName: username });
+
+      console.log("✅ User created:", user);
+      navigate("/sign-in");
     } catch (error: any) {
       console.error("❌ Error:", error.message);
     }
   };
   return (
     <div className="flex flex-col justify-evenly items-center gap-y-[20px]">
+      <input
+        type="text"
+        className="w-[340px] h-[42px] px-[16px] text-p placeholder:font-normal placeholder:text-plh rounded-[7px] border-[1px] border-plh/50 focus:outline-[1px] focus:outline-plh/70 font-medium"
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+        value={username}
+      />
+
       <input type="text"
-        className="w-[340px] h-[58px] px-[16px] text-p placeholder:font-normal placeholder:text-plh rounded-[7px] border-[1px] border-plh/50 focus:outline-[1px] focus:outline-plh/70 font-medium "
+        className="w-[340px] h-[42px] px-[16px] text-p placeholder:font-normal placeholder:text-plh rounded-[7px] border-[1px] border-plh/50 focus:outline-[1px] focus:outline-plh/70 font-medium "
         onChange={(e) => setEmail(e.target.value)}
-        placeholder="Enter Email"
+        placeholder="Email"
         value={email} />
+
       <input type="password"
-        className="w-[340px] h-[58px] px-[16px] text-p placeholder:font-normal placeholder:text-plh rounded-[7px] border-[1px] border-plh/50 focus:outline-[1px] focus:outline-plh/70 font-medium "
+        className="w-[340px] h-[42px] px-[16px] text-p placeholder:font-normal placeholder:text-plh rounded-[7px] border-[1px] border-plh/50 focus:outline-[1px] focus:outline-plh/70 font-medium "
         onChange={(e) => setPassword(e.target.value)}
-        placeholder="Enter Password"
+        placeholder="Password"
         value={password} />
+
       <button
-        className="w-[300px] h-[58px] bg-bg rounded-[30px] text-formBg font-medium text-[18px] hover:cursor-pointer hover:shadow-p/30 hover:shadow-md duration-300 transition-all"
+        className="w-[300px] h-[48px] bg-bg rounded-[30px] text-formBg font-medium text-[18px] hover:cursor-pointer hover:shadow-p/30 hover:shadow-md duration-300 transition-all"
         onClick={handleSignUp}>Sign Up</button>
 
     </div>
--- src/Components/Sign-up/WithGoogle.tsx
+++ src/Components/Sign-up/WithGoogle.tsx
@@ -16,7 +16,7 @@ function WithGoogle() {
     return (
         <div className="flex flex-col justify-evenly items-center gap-y-[20px]">
             <button onClick={signInWithGoogle}
-                className="w-[300px] h-[58px] bg-formBg rounded-[30px] border-[2px] border-bg hover:shadow-p/30 hover:shadow-md text-h1 font-medium text-[18px] hover:cursor-pointer duration-300 transition-all">Sign up with Google</button>
+                className="w-[300px] h-[48px] bg-formBg rounded-[30px] border-[2px] border-bg hover:shadow-p/30 hover:shadow-md text-h1 font-medium text-[18px] hover:cursor-pointer duration-300 transition-all">Sign up with Google</button>
             <p className="text-[15px] text-p font-medium ">Already have an account? <Link to={'/sign-in'} className="font-semibold text-h1 underline">Log in</Link></p>
         </div>
     )
