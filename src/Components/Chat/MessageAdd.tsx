158b7c1c3446a9459dc933b69c000184338a3a20
ArmenSyan <armen.sahakyan.ait@gmail.com>
2025-10-24 22:46:49 +0400

    Created Input and button for message adding

--- src/Components/Chat/MessageAdd.tsx
+++ src/Components/Chat/MessageAdd.tsx
@@ -1,7 +1,14 @@
 
 function MessageAdd() {
   return (
-    <div>MessageAdd</div>
+    <div className="lg:w-[552px] h-[55px] rounded-[16px] bg-formBg1 flex justify-evenly items-center">
+        <input 
+        type="text" 
+        className="lg:w-[350px] px-[10px] h-[36px] rounded-[10px] lg:text-[15px] font-bold bg-formBg focus:outline-plh/70 focus:outline-1  transition-all duration-200 placeholder:text-plh"
+        placeholder="Some text..."
+        />
+        <button className="lg:w-[130px] lg:h-[36px] rounded-[10px] bg-sendBtn text-[white] lg:text-[15px] lg:font-bold hover:cursor-pointer hover:bg-sendBtnHover duration-200">Send</button>
+    </div>
   )
 }
 
--- src/Components/Chat/Messages.tsx
+++ src/Components/Chat/Messages.tsx
@@ -1,7 +1,7 @@
 
 function Messages() {
   return (
-    <div className="lg:w-[552px] lg:min-h-[460px] bg-[url(/chat-bg.png)] bg-no-repeat bg-cover bg-center  rounded-[16px]">
+    <div className="lg:w-[552px] lg:min-h-[460px] bg-[url(/chat-bg.png)] bg-no-repeat bg-cover bg-center rounded-[16px]">
 
     </div>
   )
