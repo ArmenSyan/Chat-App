2bb5edaade5a64a221e8151612dc551196ad0b05
ArmenSyan <armen.sahakyan.ait@gmail.com>
2025-10-26 16:07:27 +0400

    changed the sign up page colors

--- src/Components/Sign-in/SignIn.tsx
+++ src/Components/Sign-in/SignIn.tsx
@@ -4,7 +4,7 @@ import WithEmail from "./WithEmail"
 
 function SignIn() {
   return (
-    <div className="bg-[url(/image.png)] w-full h-screen bg-cover bg-center bg-no-repeat flex justify-evenly items-center">
+    <div className="bg-gradient-to-r from-black from-10% via-purple via-45% to-black to-90% w-full h-screen bg-cover bg-center bg-no-repeat flex justify-evenly items-center">
       <div className="w-[470px] h-[520px] bg-formBg rounded-[32px] flex flex-col justify-evenly items-center">
         <div className="flex flex-col items-center gap-y-3.5 text-center">
           <h1 className="text-h1 text-[36px] font-semibold">Sign in</h1>
